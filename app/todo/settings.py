23a7410461e2def64de0a31c1e6f97d3a48f5651
solomonT <tseesolo@gmail.com>
2025-03-30 07:58:08 +0100

    Corrected render deployment

--- app/todo/settings.py
+++ app/todo/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k#fhe3x!iw)civ_sa%sn367k#ond!#hy%i!^4a3ct^gap#jeso'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
