bca5c5f33bd96f9f79ef9888b16530e507021899
solomonT <tseesolo@gmail.com>
2025-03-30 08:08:56 +0100

    Corrected render deployment

--- app/todo/settings.py
+++ app/todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-k#fhe3x!iw)civ_sa%sn367k#ond!#hy%i!^4a3ct^gap#jeso
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
